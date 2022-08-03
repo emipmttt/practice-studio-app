5f1156a88882f317b434742a9241ef95525f6591
chrischavez <chrischavez1216@gmail.com>
2022-08-03 14:54:42 -0500

    Adding styles.css, /img and making the page responsive

--- public/static/firebase.js
+++ public/static/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+
 };
 
 // Initialize Firebase
